4f40f01a6621e1991aa8b7d932614086d3d9a5cd
Atul Goyal <atulgoyal1610@gmail.com>
2026-06-26 10:10:55 +0530

    new added

---
+++ add.js
@@ -0,0 +1,12 @@
+function addNumbers() {
+      let n1 = parseFloat(document.getElementById('num1').value);
+      let n2 = parseFloat(document.getElementById('num2').value);
+      let sum = n1 + n2;
+      alert("The sum is: " + sum);
+}
+function mulNumbers() {
+      let n1 = parseFloat(document.getElementById('num1').value);
+      let n2 = parseFloat(document.getElementById('num2').value);
+      let mul = n1 * n2;
+      alert("The mul is: " + mul);
+}
